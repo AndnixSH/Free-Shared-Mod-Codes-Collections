566d92888988d48d28cb53b913ccb2027ba605e4
AndnixSH <40742924+AndnixSH@users.noreply.github.com>
2023-05-15 19:25:53 +0200

    Create Wings Of Heroes.cs

---
+++ Mobile Native Binary hex modding/Wings Of Heroes.cs
@@ -0,0 +1,5 @@
+Patched
+
+Assembly-CSharp.dll -> Game -> Gun -> ComputeMagazineRechargeTime : "1E FF 2F E1"
+Assembly-CSharp.dll -> Game -> Gun -> ComputeCooldownTime : "1E FF 2F E1"
+Assembly-CSharp.dll -> Game -> Gun -> ComputeOverheatRate : "1E FF 2F E1"
